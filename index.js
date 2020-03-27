7311116b9d1c2cc495713f61ff62d65a0a3fe913
Lauren Yu <laurenkyu@gmail.com>
2020-03-27 18:27:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 class Formatter {
   static capitalize(string) {
     let array = string.split("")
-    array[0]
+    array[0].toUpperCase()
+    array.join("")
+    return array
   }
 }
 
+console.log(Formatter.capitalize("hello"))
